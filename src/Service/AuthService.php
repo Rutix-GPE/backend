483e68a90adca9170cab8e6aa9ce9a80ab3e0c25
achatt_m <achatt_m@etna-alternance.net>
2025-05-19 14:20:39 +0200

    add Dto Services Crud user

--- src/Service/AuthService.php
+++ src/Service/AuthService.php
@@ -6,6 +6,7 @@ namespace App\Service;
 use App\Entity\User;
 use App\Dto\Auth\UserLoginDto;
 use App\Dto\UserResponseDTO;
+use App\Dto\Auth\UserRegisterDTO;
 use App\Repository\UserRepository;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -39,6 +40,25 @@ class AuthService
             'token' => $token,
             'user' => $userDto
         ];
+    }
+    public function register(UserRegisterDTO $dto): User
+    {
+        $user = new User();
+        $user->setUsername($dto->username);
+        $user->setFirstname($dto->firstname);
+        $user->setLastname($dto->lastname);
+        $user->setEmail($dto->email);
+        $user->setPassword($this->passwordHasher->hashPassword($user, $dto->password));
+
+        $user->setPhonenumber($dto->phonenumber);
+        $user->setCountry($dto->country);
+        $user->setPostalcode($dto->postalcode);
+        $user->setCity($dto->city);
+        $user->setAdress($dto->adress);
+
+        $this->userRepository->add($user, true);
+
+        return $user;
     }
     
 }
--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -1,20 +1,64 @@
-<?php 
-
+<?php
 namespace App\Service;
 
-use App\Entity\TemplateQuestion;
-use App\Repository\TemplateQuestionRepository;
 use App\Repository\UserRepository;
-use App\Repository\UserResponseRepository;
+use App\Dto\UserResponseDTO;
+use App\Dto\UserInputDTO;
 
-class UserService 
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
+class UserService
 {
+    public function __construct(
+        private UserRepository $userRepository,
+        private UserPasswordHasherInterface $passwordHasher,
 
-    private UserRepository $userRepository;
-    
-    public function __construct(UserRepository $userRepository) 
+    ) {}
+
+    public function getUserById(int $id): UserResponseDTO
     {
-        $this->userRepository = $userRepository;
+        $user = $this->userRepository->find($id);
+
+        if (!$user) {
+            throw new NotFoundHttpException("User not found");
+        }
+
+        return new UserResponseDTO($user);
     }
 
+    public function getAllUsers(): array
+    {
+        $users = $this->userRepository->findAll();
+
+        return array_map(fn($user) => new UserResponseDTO($user), $users);
+    }
+
+    public function updateUser(int $id, UserInputDTO $dto): UserResponseDTO
+    {
+        $user = $this->userRepository->find($id);
+        if (!$user) {
+            throw new NotFoundHttpException('User not found');
+        }
+        // Mise à jour conditionnelle
+        if ($dto->username)     $user->setUsername($dto->username);
+        if ($dto->firstname)    $user->setFirstname($dto->firstname);
+        if ($dto->lastname)     $user->setLastname($dto->lastname);
+        if ($dto->email)        $user->setEmail($dto->email);
+        if ($dto->phonenumber)  $user->setPhonenumber($dto->phonenumber);
+        if ($dto->country)      $user->setCountry($dto->country);
+        if ($dto->postalcode)   $user->setPostalcode($dto->postalcode);
+        if ($dto->city)         $user->setCity($dto->city);
+        if ($dto->adress)       $user->setAdress($dto->adress);
+        if ($dto->memo)         $user->setMemo($dto->memo);
+        if ($dto->password) {
+            $hashed = $this->passwordHasher->hashPassword($user, $dto->password);
+            $user->setPassword($hashed);
+        }
+
+        $this->userRepository->add($user, true);
+
+        return new UserResponseDTO($user);
+    }
 }
