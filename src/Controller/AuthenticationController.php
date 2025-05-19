b421ba1141e48ed4d1368a5ede98badee93c02ee
achatt_m <achatt_m@etna-alternance.net>
2025-05-19 12:50:02 +0200

    service et DTO Auth Service

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -1,9 +1,14 @@
 <?php
 
 namespace App\Controller;
-
 use App\Entity\User;
 use App\Repository\UserRepository;
+use App\Dto\Auth\UserRegisterDTO;
+use App\Dto\Auth\UserLoginDTO;
+use App\Dto\UserResponseDTO;
+use App\Service\RegisterService;
+use App\Service\AuthService;
+
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -11,9 +16,46 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class AuthenticationController extends AbstractController
 {
+    #[Route('/user/register', name: 'user_register', methods: ['POST'])]
+    public function register(
+        Request $request,
+        SerializerInterface $serializer,
+        ValidatorInterface $validator,
+        RegisterService $registerService
+    ): JsonResponse
+    {
+        try {
+            /** @var ConstraintViolationListInterface $errors */
+            $dto = $serializer->deserialize($request->getContent(), UserRegisterDTO::class, 'json');
+            $errors = $validator->validate($dto);
+
+            $errors = $validator->validate($dto);
+
+            if (count($errors) > 0) {
+                $errorMessages = [];
+                foreach ($errors as $error) {
+                    $errorMessages[$error->getPropertyPath()] = $error->getMessage();
+                }
+    
+                return new JsonResponse(['errors' => $errorMessages], JsonResponse::HTTP_BAD_REQUEST);
+            }
+        
+            $user = $registerService->register($dto);
+            $userDto = new UserResponseDTO($user);
+    
+            return $this->json($userDto, Response::HTTP_CREATED);
+    
+        } catch (\Throwable $e) {
+            return $this->json(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    /*
     #[Route('/user/register', name: 'user_register')]
     public function register(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher): JsonResponse
     {
@@ -92,50 +134,24 @@ class AuthenticationController extends AbstractController
             $response = ["error" => $error->getMessage()];
             return $this->json($response, Response::HTTP_BAD_REQUEST);
         }
-    }
-
-
-    #[Route('/user/authenticate', name: 'user_authenticate')]
-    public function authenticate(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher, JWTTokenManagerInterface $tokenManger): JsonResponse
+    }*/
+    #[Route('/user/authenticate', name: 'user_authenticate', methods: ['POST'])]
+    public function authenticate(Request $request, AuthService $authService): JsonResponse
     {
-        $data = $request->getContent();
-        $data = json_decode($data, true);
+        try {
+            $data = json_decode($request->getContent(), true);
+            $dto = new UserLoginDTO($data);
 
-        
-        if (!isset($data['password']) || (!isset($data['email']) && !isset($data['username']))) {
-            $response = ["error" => "Missing informations"];
-            return $this->json($response, Response::HTTP_UNAUTHORIZED);
-        }
-        
-        if(isset($data['email']) && isset($data['username'])){
-            $response = ["error" => "Email and Username detected, only one accepted"];
-            return $this->json($response, Response::HTTP_UNAUTHORIZED);
-        }
-        
-        if(isset($data['email']))
-        {
-            $user = $userRepository->findOneBy(['email' => $data['email']]);
-        }
-        elseif(isset($data['username']))
-        {
-            $user = $userRepository->findOneBy(['username' => $data['username']]);
-        }
+            $authResult = $authService->authenticate($dto);
 
-        if(!$user){
-            $response = ["error" => "Wrong password / username / email"];
-            return $this->json($response, Response::HTTP_NOT_FOUND);
+            return $this->json([
+                'token' => $authResult['token'],
+                'user' => get_object_vars($authResult['user']) // ou $this->normalizer->normalize(...)
+            ], Response::HTTP_OK);
+        } catch (\InvalidArgumentException $e) {
+            return $this->json(["error" => $e->getMessage()], Response::HTTP_BAD_REQUEST);
+        } catch (\Exception $e) {
+            return $this->json(["error" => "Invalid email/username or password"], Response::HTTP_UNAUTHORIZED);
         }
-        
-        if(!$passwordHasher->isPasswordValid($user, $data['password'])){
-            $response = ["error" => "Wrong password / username / email"];
-            return $this->json($response, Response::HTTP_NOT_FOUND);
-        }
-
-        $token = $tokenManger->create($user);
-
-        $response = ["token" => $token];
-
-        return $this->json($response, Response::HTTP_OK);
     }
-
 }
