8b886b114db0e7dc0d35e227023e5e28b1f7c774
achatt_m <achatt_m@etna-alternance.net>
2025-05-26 23:22:07 +0200

    rebase dev en featuredto

--- migrations/Version20250526195527.php
+++ migrations/Version20250526195527.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250526174906 extends AbstractMigration
+final class Version20250526195527 extends AbstractMigration
 {
     public function getDescription(): string
     {
