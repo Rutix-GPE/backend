32f1276cca73fd67d09f5c0f3c6c3157de6bfa80
Arthur Rubiralta <onyx.pixart@gmail.com>
2025-07-30 19:24:06 +0200

    .

--- migrations/Version20250730171713.php
+++ migrations/Version20250730171713.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250730171029 extends AbstractMigration
+final class Version20250730171713 extends AbstractMigration
 {
     public function getDescription(): string
     {
